53e4514f682a3f0040a59bfe11c3c5278ad5865d
Liptom328 <74043742+Liptom328@users.noreply.github.com>
2022-08-12 22:25:30 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lego DB', data: [ { id: 1, name: "Red Brick" }, { id: 2, name: "Yellow Brick" }, { id: 3, name: "Green Brick" }, ] });
+  res.render('index', { title: 'Lego DB', data: [ { id: 1, name: "Red Brick", url: "https://www.turbosquid.com/pl/3d-models/3d-model-lego-brick-2x2-bright-1409481" }, { id: 2, name: "Yellow Brick", url: "https://www.turbosquid.com/pl/3d-models/3d-model-lego-brick-2x2-bright-1409481" }, { id: 3, name: "Green Brick", url: "https://www.turbosquid.com/pl/3d-models/3d-model-lego-brick-2x2-bright-1409481" }, ] });
 });
 
 module.exports = router;
