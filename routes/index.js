9a9125ad0947750ffe52d5aa6810d6250e298d51
Liptom328 <74043742+Liptom328@users.noreply.github.com>
2022-08-12 22:29:21 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lego DB', data: [ { id: 1, name: "Red Brick", url: "https://www.turbosquid.com/pl/3d-models/3d-model-lego-brick-2x2-bright-1409481" }, { id: 2, name: "Yellow Brick", url: "https://www.turbosquid.com/pl/3d-models/3d-model-lego-brick-2x2-bright-1409481" }, { id: 3, name: "Green Brick", url: "https://www.turbosquid.com/pl/3d-models/3d-model-lego-brick-2x2-bright-1409481" }, ] });
+  res.render('index', { title: 'Lego DB', data: [ { id: 1, name: "Red Brick", url: "https://static.turbosquid.com/Preview/2019/05/23__07_35_00/Lego_Brick_2x2_Bright_Red_thumbnail_Square_0000.jpg1B65A82A-AF38-438A-B488-761044B6A61FLarge.jpg" }, { id: 2, name: "Yellow Brick", url: "https://static.turbosquid.com/Preview/2019/05/23__07_35_00/Lego_Brick_2x2_Bright_Red_thumbnail_Square_0000.jpg1B65A82A-AF38-438A-B488-761044B6A61FLarge.jpg" }, { id: 3, name: "Green Brick", url: "https://static.turbosquid.com/Preview/2019/05/23__07_35_00/Lego_Brick_2x2_Bright_Red_thumbnail_Square_0000.jpg1B65A82A-AF38-438A-B488-761044B6A61FLarge.jpg" }, ] });
 });
 
 module.exports = router;
